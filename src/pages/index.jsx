b184593009a4e5a54634f797444ae53e6bcf2549
KODIGIT <49615013+kodigit1@users.noreply.github.com>
2023-12-28 19:06:22 +0100

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SEO from "../common/seo";
-import HomeOne from "../components/homes/home";
+import HomeTwo from "../components/homes/home-2";
 import Wrapper from "../layout/wrapper";
 
 const Home = () => {
