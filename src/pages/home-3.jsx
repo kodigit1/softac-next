8f34a58347646b733170583c90bbee3eca93971e
KODIGIT <49615013+kodigit1@users.noreply.github.com>
2024-04-02 18:20:25 +0200

    Update home-3.jsx

--- src/pages/home-3.jsx
+++ src/pages/home-3.jsx
@@ -6,7 +6,7 @@ import Wrapper from "../layout/wrapper";
 const index = () => {
   return (
     <Wrapper>
-      <SEO pageTitle={"Softec - Data analytics"} />
+      <SEO pageTitle={"Kodigit Beauty - gestione appuntamenti per Centri Estetici e Hair Stylist"} />
       <HomeThree />
     </Wrapper>
   );
