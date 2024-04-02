cf0835d371fa7979e91aea67b7ce55dda1e91e32
KODIGIT <49615013+kodigit1@users.noreply.github.com>
2024-04-02 17:54:24 +0200

    Update index.jsx

--- src/components/project/index.jsx
+++ src/components/project/index.jsx
@@ -1,5 +1,5 @@
 import BreadcrumbSeven from "@/src/common/breadcrumbs/breadcrumb-7";
-import FooterFive from "@/src/layout/footers/footer-5";
+import FooterThree from "@/src/layout/footers/footer-3";
 import HeaderSix from "@/src/layout/headers/header-6";
 import React from "react";
 import CtaArea from "../contact/cta-area";
