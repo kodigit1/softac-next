79a04f4555063cec5ac55de295dfdb57c313584a
KODIGIT <49615013+kodigit1@users.noreply.github.com>
2024-04-04 10:45:02 +0200

    Update header-6.jsx

--- src/layout/headers/header-6.jsx
+++ src/layout/headers/header-6.jsx
@@ -50,8 +50,8 @@ const HeaderSix = ({ style_2 = false }) => {
                               </Link>
                            </div>
                            <div className="header-bottom__btn d-flex align-items-center">
-                              <Link className={`${style_2 ? 'tp-btn-inner alt-color-orange' : 'tp-btn-white alt-color-black'} tp-btn-hover d-none d-md-inline-block`} href="/service-details">
-                                 <span className="white-text">Get Started</span>
+                              <Link className={`${style_2 ? 'tp-btn-inner alt-color-orange' : 'tp-btn-white alt-color-black'} tp-btn-hover d-none d-md-inline-block`} href="/contact">
+                                 <span className="white-text">Chiama Ora !</span>
                                  <b></b>
                               </Link>
                               <a className="header-bottom__bar tp-menu-bar d-lg-none" onClick={() => setSidebarOpen(true)}>
